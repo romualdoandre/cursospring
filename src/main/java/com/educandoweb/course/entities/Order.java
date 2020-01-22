0222b3239b88d3552865ac8063b102a0923bc483
romualdoandre <romualdoandre@gmail.com>
2020-01-22 17:34:11 -0300

    payment

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
@@ -33,6 +35,8 @@ public class Order implements Serializable {
 	private Integer orderStatus;
 	@OneToMany(mappedBy="id.order")
 	private Set<OrderItem> items;
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 		items=new HashSet<OrderItem>();
@@ -78,6 +82,14 @@ public class Order implements Serializable {
 	public void setOrderStatus(OrderStatus orderStatus) {
 		if(orderStatus!=null)
 			this.orderStatus = orderStatus.getCode();
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Set<OrderItem> getItems() {
